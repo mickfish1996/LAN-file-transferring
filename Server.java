87201dc2b6285b69938fa3f0002808c3738d446e
mickfish1996 <fis17005@byui.edu>
2022-11-09 14:10:11 -0700

    added to the server

--- Server.java
+++ Server.java
@@ -13,5 +13,25 @@ class Server{
       clientSocket = serverSocket.accept();
       out = new PrintWriter(clientSocket.getOutputStream(), true);
       in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+      String greeting = in.readLine();
+      
+      if ("hello server".equals(greeting))
+      {
+         out.println("hello client");
+      }
+      else {
+         out.println("unrecognised greeting");
+      }
    }
+
+   public void stop() throws IOException
+   {
+      in.close();
+      out.close();
+      clientSocket.close();
+      serverSocket.close();
+   }
+
+   
 }
